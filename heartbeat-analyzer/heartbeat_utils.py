07693575cdd6e11432528a518a2eb005aa1fcd1c
Shomik Mukhopadhyay <kimosh539@gmail.com>
2025-08-12 23:04:46 -0500

    Add plotting function and improve error handling

--- heartbeat-analyzer/heartbeat_utils.py
+++ heartbeat-analyzer/heartbeat_utils.py
@@ -1,7 +1,7 @@
 #Add function to read heartbeat data
 
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 def read_heartbeat_csv(filepath):
     """
@@ -14,3 +14,27 @@ def read_heartbeat_csv(filepath):
     except FileNotFoundError:
         print(f"file not found: {filepath}")
         return None
+
+
+def plot_heartbeat(df):
+    """
+    Plots heartbeat signal from a DataFrame with 'Time (s)' and 'Voltage (mV)' columns.
+    """
+    try:
+        # Getting the column names from data file
+        x_col = df.columns[0]
+        y_col = df.columns[1]
+
+        plt.figure(figsize=(10,4))
+        #plt.plot(df["Time (s)"],df["Voltage (mV)"],label='HeartBeat signal')
+        plt.plot(df.iloc[:,0], df.iloc[:,1],label='HeartBeat signal')
+        plt.xlabel(x_col)
+        plt.ylabel(y_col)
+        #plt.ylabel(""Voltage (mV)")
+        plt.title("HeartBeat signal plot")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    except Exception as e:
+        print("Data not in plottable format")
--- heartbeat-analyzer/main.py
+++ heartbeat-analyzer/main.py
@@ -1,10 +1,23 @@
 # Main Code
 
 from heartbeat_utils import read_heartbeat_csv
+from heartbeat_utils import plot_heartbeat
 
 
 # Capture ECG data from sheet and convert to dataframe
 
 df = read_heartbeat_csv("data/sample_heartbeat.csv")
+
+
 if df is not None:
+    # Print starting rows of the ECG data
     print(df.head())
+
+    # Plot the dataframe ECG data as voltage vs time graph
+    plot_heartbeat(df)
+
+
+
+
+
+
